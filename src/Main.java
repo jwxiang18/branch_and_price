580e73465f58fc50e6543b62681122d7ff0f2458
Jwxiang <xiangshi2021@163.com>
2023-04-09 22:14:24 +0800

    更新数据集

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException, GRBException, CloneNotSupportedException {
         double start = System.currentTimeMillis();
-        Instance instance = ReadData.readData("R101.txt" , 100);
+        Instance instance = ReadData.readData("C101.txt" , 100);
 //        CG cg = new CG(instance);
 //        cg.MP();
 
